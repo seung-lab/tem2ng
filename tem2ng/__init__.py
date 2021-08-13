091a4c9f48d8e652841050160db44bc7a391e420
William Silversmith <william.silversmith@gmail.com>
2021-08-13 15:32:09 -0400

    fix: update mapping with Zhihao's recommendation
    Columns are 24 supertiles until supertile 288 when they are 23 tall.

--- tem2ng/__init__.py
+++ tem2ng/__init__.py
@@ -14,13 +14,15 @@ import cv2
 TILE_REGEXP = re.compile(r'tile_(\d+)_(\d+)\.bmp')
 
 def get_ng(tilename, z=0):
-	x_map = {4:0,5:1,6:2,3:0,0:1,7:2,2:0,1:1,8:2}
-	get_x = lambda t1,t2: 6000 * ((t1%24)*3 + x_map[t2])
-	y_map = {4:0,3:1,2:2,5:0,0:1,1:2,6:0,7:1,8:2}
-	get_y = lambda t1,t2: 6000 * ((t1//24)*3 + y_map[t2])
-
 	t1, t2 = [ int(_) for _ in re.search(TILE_REGEXP, tilename).groups() ]
 	
+	column_size = 23 if t1 >= 288 else 24
+
+	x_map = {4:0,5:1,6:2,3:0,0:1,7:2,2:0,1:1,8:2}
+	get_x = lambda t1,t2: 6000 * ((t1%column_size)*3 + x_map[t2])
+	y_map = {4:0,3:1,2:2,5:0,0:1,1:2,6:0,7:1,8:2}
+	get_y = lambda t1,t2: 6000 * ((t1//column_size)*3 + y_map[t2])
+
 	x0 = get_x(t1, t2)
 	xf = x0 + 6000
 	y0 = get_y(t1,t2)
