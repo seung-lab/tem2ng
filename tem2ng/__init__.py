49d9b54bab9f9cb9faa453c647c23bd7d55bb10c
William Silversmith <william.silversmith@gmail.com>
2021-08-13 15:57:43 -0400

    chore: use spaces instead of tabs

--- tem2ng/__init__.py
+++ tem2ng/__init__.py
@@ -15,21 +15,21 @@ from cloudvolume.lib import mkdir, touch
 TILE_REGEXP = re.compile(r'tile_(\d+)_(\d+)\.bmp')
 
 def get_ng(tilename, z=0):
-	t1, t2 = [ int(_) for _ in re.search(TILE_REGEXP, tilename).groups() ]
-	
-	column_size = 23 if t1 >= 288 else 24
+    t1, t2 = [ int(_) for _ in re.search(TILE_REGEXP, tilename).groups() ]
+    
+    column_size = 23 if t1 >= 288 else 24
 
-	x_map = {4:0,5:1,6:2,3:0,0:1,7:2,2:0,1:1,8:2}
-	get_x = lambda t1,t2: 6000 * ((t1%column_size)*3 + x_map[t2])
-	y_map = {4:0,3:1,2:2,5:0,0:1,1:2,6:0,7:1,8:2}
-	get_y = lambda t1,t2: 6000 * ((t1//column_size)*3 + y_map[t2])
+    x_map = {4:0,5:1,6:2,3:0,0:1,7:2,2:0,1:1,8:2}
+    get_x = lambda t1,t2: 6000 * ((t1%column_size)*3 + x_map[t2])
+    y_map = {4:0,3:1,2:2,5:0,0:1,1:2,6:0,7:1,8:2}
+    get_y = lambda t1,t2: 6000 * ((t1//column_size)*3 + y_map[t2])
 
-	x0 = get_x(t1, t2)
-	xf = x0 + 6000
-	y0 = get_y(t1,t2)
-	yf = y0 + 6000
+    x0 = get_x(t1, t2)
+    xf = x0 + 6000
+    y0 = get_y(t1,t2)
+    yf = y0 + 6000
 
-	return f"{x0}-{xf}_{y0}-{yf}_{z}-{z+1}"
+    return f"{x0}-{xf}_{y0}-{yf}_{z}-{z+1}"
 
 class Tuple3(click.ParamType):
   """A command line option type consisting of 3 comma-separated integers."""
@@ -47,11 +47,11 @@ class Tuple3(click.ParamType):
 
 @click.group()
 def main():
-	"""
-	Upload tiles from a specific setup at PNI to
-	cloud storage.
-	"""
-	pass
+    """
+    Upload tiles from a specific setup at PNI to
+    cloud storage.
+    """
+    pass
 
 @main.command()
 @click.option('--dataset-size', type=Tuple3(), default=None, required=True, help="Dimensions of the dataset in voxels.")
@@ -62,66 +62,66 @@ def main():
 @click.option('--num-mips', type=int, default=1, help="Number of mip levels to generate at once.", show_default=True)
 @click.argument("cloudpath")
 def info(
-	cloudpath, 
-	dataset_size, voxel_offset, chunk_size, 
-	resolution, bit_depth, num_mips
+    cloudpath, 
+    dataset_size, voxel_offset, chunk_size, 
+    resolution, bit_depth, num_mips
 ):
-	"""
-	Creates and uploads the neuroglancer info file.
-	This defines the size and properties of the image.
-	We assume a grayscale image for microscopes.
-	"""
-	if bit_depth not in (8,16,32,64):
-		print("tem2ng: bit depth must be 8, 16, 32, or 64.")
+    """
+    Creates and uploads the neuroglancer info file.
+    This defines the size and properties of the image.
+    We assume a grayscale image for microscopes.
+    """
+    if bit_depth not in (8,16,32,64):
+        print("tem2ng: bit depth must be 8, 16, 32, or 64.")
 
-	info = CloudVolume.create_new_info(
-	    num_channels    = 1,
-	    layer_type      = 'image',
-	    data_type       = f'uint{bit_depth}',
-	    encoding        = 'raw', 
-	    resolution      = resolution, # Voxel scaling, units are in nanometers
-	    voxel_offset    = voxel_offset, # x,y,z offset in voxels from the origin
-	    # Pick a convenient size for your underlying chunk representation
-	    # Powers of two are recommended, doesn't need to cover image exactly
-	    chunk_size      = chunk_size, # units are voxels
-	    volume_size     = dataset_size, # e.g. a cubic millimeter dataset
-	)
-	cv = CloudVolume(cloudpath, info=info)
+    info = CloudVolume.create_new_info(
+        num_channels    = 1,
+        layer_type      = 'image',
+        data_type       = f'uint{bit_depth}',
+        encoding        = 'raw', 
+        resolution      = resolution, # Voxel scaling, units are in nanometers
+        voxel_offset    = voxel_offset, # x,y,z offset in voxels from the origin
+        # Pick a convenient size for your underlying chunk representation
+        # Powers of two are recommended, doesn't need to cover image exactly
+        chunk_size      = chunk_size, # units are voxels
+        volume_size     = dataset_size, # e.g. a cubic millimeter dataset
+    )
+    cv = CloudVolume(cloudpath, info=info)
 
-	for mip in range(1, num_mips):
-		cv.add_scale([2 ** mip, 2 ** mip, 1])
+    for mip in range(1, num_mips):
+        cv.add_scale([2 ** mip, 2 ** mip, 1])
 
-	cv.commit_info()
+    cv.commit_info()
 
 @main.command()
 @click.argument("source")
 @click.argument("destination")
 def upload(source, destination):
-	"""
-	Process a subtile directory and upload to
-	cloud storage.
-	"""
-	vol = CloudVolume(destination)
-	progress_dir = mkdir(os.path.join(source, 'progress'))
+    """
+    Process a subtile directory and upload to
+    cloud storage.
+    """
+    vol = CloudVolume(destination)
+    progress_dir = mkdir(os.path.join(source, 'progress'))
 
-	done_files = set(os.listdir(progress_dir))
-	all_files = os.listdir(source)
-	all_files = set([ 
-		fname for fname in all_files 
-		if os.path.splitext(fname)[1] != "bmp" 
-	])
-	to_upload = list(all_files.difference(done_files))
-	to_upload.sort()
+    done_files = set(os.listdir(progress_dir))
+    all_files = os.listdir(source)
+    all_files = set([ 
+        fname for fname in all_files 
+        if os.path.splitext(fname)[1] != "bmp" 
+    ])
+    to_upload = list(all_files.difference(done_files))
+    to_upload.sort()
 
-	def process(filename):
-		img = cv2.imread(os.path.join(source, filename), cv2.IMREAD_GRAYSCALE)
-		while img.ndim < 4:
-			img = img[..., np.newaxis]
+    def process(filename):
+        img = cv2.imread(os.path.join(source, filename), cv2.IMREAD_GRAYSCALE)
+        while img.ndim < 4:
+            img = img[..., np.newaxis]
 
-		bbx = Bbox.from_filename(get_ng(filename))
-		vol[bbx] = img
-		touch(os.path.join(progress_dir, filename))
-		return 1
+        bbx = Bbox.from_filename(get_ng(filename))
+        vol[bbx] = img
+        touch(os.path.join(progress_dir, filename))
+        return 1
 
   with tqdm(desc="Upload", total=total) as pbar:
     with pathos.pools.ProcessPool(parallel) as pool:
