2cbb3e9eac8e3be7848c1c2a2b785a85555efd38
zhihaozheng <zzhengcn@gmail.com>
2021-09-12 16:07:14 -0400

    edit to upload aw stitched montage

--- tem2ng/__init__.py
+++ tem2ng/__init__.py
@@ -9,44 +9,23 @@ import numpy as np
 import pathos.pools
 from tqdm import tqdm
 import tinybrain
+from PIL import Image
 
 from cloudvolume import CloudVolume, Bbox
 from cloudvolume.exceptions import InfoUnavailableError
 from cloudvolume.lib import mkdir, touch
 
-TILE_REGEXP = re.compile(r'tile_(\d+)_(\d+)\.bmp')
+TILE_REGEXP = re.compile(r'SuperSuperTile_(\d+)_(\d+)\.tif')
 
-x_step = 42320
-y_step = 42309
-
-def get_ng(tilename, x, y, z=0):
+def get_ng(tilename, z=0):
     t1, t2 = [ int(_) for _ in re.search(TILE_REGEXP, tilename).groups() ]
 
-    x_map = {6:0,7:1,8:2,5:0,0:1,1:2,4:0,3:1,2:2}
-    get_x = lambda t2: 6000 * (round(x / x_step)*3 + x_map[t2])
-    y_map = {6:0,5:1,4:2,7:0,0:1,3:2,8:0,1:1,2:2}
-    get_y = lambda t2: 6000 * ((50 - round(y / y_step))*3 + y_map[t2])
-
-    x0 = get_x(t2)
-    xf = x0 + 6000
-    y0 = get_y(t2)
-    yf = y0 + 6000
+    x0 = (t2-1)*65000
+    xf = x0 + 65000
+    y0 = (t1-1)*65000
+    yf = y0 + 65000
 
     return f"{x0}-{xf}_{y0}-{yf}_{z}-{z+1}"
-
-def read_stage(path):
-    with open(path) as f:
-        lines = f.readlines()
-    return float(lines[10].split(" = ")[1]), float(lines[11].split(" = ")[1])
-
-def read_stage_csv(file="metadata/stage_positions.csv"):
-    stage_csv = []
-    with open(file) as f:
-        reader = csv.reader(f, delimiter=',')
-        next(reader)
-        for row in reader:
-            stage_csv.append([float(row[1]),float(row[2])])
-    return stage_csv
 
 class Tuple3(click.ParamType):
   """A command line option type consisting of 3 comma-separated integers."""
@@ -120,10 +99,8 @@ def info(
 @click.argument("source")
 @click.argument("destination")
 @click.option('--z', type=int, default=0, help="Z coordinate to upload this section to.", show_default=True)
-@click.option('--bottom_tile', type=int, default=0, help="bottom most tile", show_default=True)
-@click.option('--midleft_tile', type=int, default=62, help="middle tile leftmost", show_default=True)
 @click.pass_context
-def upload(ctx, source, destination, z, bottom_tile, midleft_tile):
+def upload(ctx, source, destination, z):
     """
     Process a subtile directory and upload to
     cloud storage.
@@ -131,18 +108,13 @@ def upload(ctx, source, destination, z, bottom_tile, midleft_tile):
     vol = CloudVolume(destination)
     progress_dir = mkdir(os.path.join(source, 'progress'))
 
-    stage_csv = read_stage_csv()
-
-    south_most = stage_csv[bottom_tile][1] - y_step*24
-    west_most = stage_csv[midleft_tile][0] - x_step*5
-
     done_files = set(os.listdir(progress_dir))
     all_files = os.listdir(source)
     all_files = set([
         fname for fname in all_files
         if (
             os.path.isfile(os.path.join(source, fname))
-            and os.path.splitext(fname)[1] == ".bmp"
+            and os.path.splitext(fname)[1] == ".tif"
         )
     ])
 
@@ -150,17 +122,12 @@ def upload(ctx, source, destination, z, bottom_tile, midleft_tile):
     to_upload.sort()
 
     def process(filename, west_most, south_most, stage_csv):
-        img = cv2.imread(os.path.join(source, filename), cv2.IMREAD_GRAYSCALE)
-        img = cv2.transpose(img)
+        im = Image.open(os.path.join(source, filename))
+        img = np.array(im)
         while img.ndim < 4:
             img = img[..., np.newaxis]
 
-        stage_x = west_most
-        stage_y = south_most
-
-        stages = stage_csv[int(filename.split("_")[1])]
-
-        bbx = Bbox.from_filename(get_ng(filename, stages[0]-west_most, stages[1]-south_most, z=z))
+        bbx = Bbox.from_filename(get_ng(filename, z=z))
         vol[bbx] = img
         touch(os.path.join(progress_dir, filename))
         return 1
@@ -169,5 +136,5 @@ def upload(ctx, source, destination, z, bottom_tile, midleft_tile):
 
     with tqdm(desc="Upload", total=len(all_files), initial=len(done_files)) as pbar:
         with pathos.pools.ProcessPool(parallel) as pool:
-            for num_inserted in pool.imap(lambda x: process(x, west_most, south_most, stage_csv), to_upload):
+            for num_inserted in pool.imap(lambda x: process(x), to_upload):
                 pbar.update(num_inserted)
