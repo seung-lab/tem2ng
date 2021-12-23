97929762feec5c50d8821f0de5a155890e562e2a
zhihaozheng <zzhengcn@gmail.com>
2021-12-23 09:38:17 -0500

    stitch upload to tigerdata

--- tem2ng/__init__.py
+++ tem2ng/__init__.py
@@ -13,24 +13,14 @@ from cloudvolume import CloudVolume, Bbox
 from cloudvolume.exceptions import InfoUnavailableError
 from cloudvolume.lib import mkdir, touch
 
-TILE_REGEXP = re.compile(r'tile_(\d+)_(\d+)\.bmp')
+TILE_REGEXP = re.compile(r'c(\d+)r(\d+)\.tif')
 
 def get_ng(tilename, z=0):
     t1, t2 = [ int(_) for _ in re.search(TILE_REGEXP, tilename).groups() ]
+    x0 = (t1-1)*2000
+    y0 = (t2-1)*2000
 
-    col_height = 23 if t1 >= 288 else 24
-
-    x_map = {6:0,7:1,8:2,5:0,0:1,1:2,4:0,3:1,2:2}
-    get_x = lambda t1,t2: 6000 * ((t1//col_height)*3 + x_map[t2])
-    y_map = {6:0,5:1,4:2,7:0,0:1,3:2,8:0,1:1,2:2}
-    get_y = lambda t1,t2: 6000 * ((col_height-1-t1%col_height)*3 + y_map[t2])
-
-    x0 = get_x(t1, t2)
-    xf = x0 + 6000
-    y0 = get_y(t1,t2)
-    yf = y0 + 6000
-
-    return f"{x0}-{xf}_{y0}-{yf}_{z}-{z+1}"
+    return f"{x0}-{x0+2000}_{y0}-{y0+2000}_{z}-{z+1}"
 
 class Tuple3(click.ParamType):
   """A command line option type consisting of 3 comma-separated integers."""
@@ -119,7 +109,7 @@ def upload(ctx, source, destination, z):
         fname for fname in all_files
         if (
             os.path.isfile(os.path.join(source, fname))
-            and os.path.splitext(fname)[1] == ".bmp"
+            and os.path.splitext(fname)[1] == ".tif"
         )
     ])
     to_upload = list(all_files.difference(done_files))
@@ -127,7 +117,7 @@ def upload(ctx, source, destination, z):
 
     def process(filename):
         img = cv2.imread(os.path.join(source, filename), cv2.IMREAD_GRAYSCALE)
-        img = cv2.transpose(img)
+        # img = cv2.transpose(img)
         while img.ndim < 4:
             img = img[..., np.newaxis]
 
